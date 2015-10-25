e9a883ac3fe426fb39b0c96618cfa18097a20b04
Armour <497052684@qq.com>
2015-10-25 01:18:10 -0700

    formated code

--- AES.c
+++ AES.c
@@ -12,44 +12,45 @@ unsigned char IV[] = "\x0A\x91\x72\x71\x6A\xE6\x42\x84\x09\x88\x5B\x8B\x82\x9C\x
 AES_KEY key;
 
 void encrypt(void) {
-	FILE *ifp, *ofp;
-	ifp = fopen("signature", "r+");
-	ofp = fopen("encrypted", "w+");
-	int postion = 0;
-	int bytes_read, bytes_write;
-	while (1) {
-		unsigned char ivec[AES_BLOCK_SIZE];
-		memcpy(ivec, IV, AES_BLOCK_SIZE);
-		bytes_read = fread(indata, 1, AES_BLOCK_SIZE, ifp);
-		AES_cfb128_encrypt(indata, outdata, bytes_read, &key, ivec, &postion, AES_ENCRYPT);
-		bytes_write = fwrite(outdata, 1, bytes_read, ofp);
-	    if (bytes_read < AES_BLOCK_SIZE)
-			break;
-	}
-	fclose(ifp);
-	fclose(ofp);
+    FILE *ifp, *ofp;
+    ifp = fopen("signature", "r+");
+    ofp = fopen("encrypted", "w+");
+    int postion = 0;
+    int bytes_read, bytes_write;
+    while (1) {
+        unsigned char ivec[AES_BLOCK_SIZE];
+        memcpy(ivec, IV, AES_BLOCK_SIZE);
+        bytes_read = fread(indata, 1, AES_BLOCK_SIZE, ifp);
+        AES_cfb128_encrypt(indata, outdata, bytes_read, &key, ivec, &postion, AES_ENCRYPT);
+        bytes_write = fwrite(outdata, 1, bytes_read, ofp);
+        if (bytes_read < AES_BLOCK_SIZE)
+            break;
+    }
+    fclose(ifp);
+    fclose(ofp);
 }
+
 void decrypt(void) {
-	FILE *ifp, *ofp;
-	ifp = fopen("encrypted", "r+");
-	ofp = fopen("decrypted", "w+");
-	int postion = 0;
-	int bytes_read, bytes_write;
-	while (1) {
-		unsigned char ivec[AES_BLOCK_SIZE];
-		memcpy(ivec, IV, AES_BLOCK_SIZE);
-		bytes_read = fread(outdata, 1, AES_BLOCK_SIZE, ifp);
-		AES_cfb128_encrypt(outdata, decryptdata, bytes_read, &key, ivec, &postion, AES_DECRYPT);
-		bytes_write = fwrite(decryptdata, 1, bytes_read, ofp);
-	    if (bytes_read < AES_BLOCK_SIZE)
-			break;
-	}
-	fclose(ifp);
-	fclose(ofp);
+    FILE *ifp, *ofp;
+    ifp = fopen("encrypted", "r+");
+    ofp = fopen("decrypted", "w+");
+    int postion = 0;
+    int bytes_read, bytes_write;
+    while (1) {
+        unsigned char ivec[AES_BLOCK_SIZE];
+        memcpy(ivec, IV, AES_BLOCK_SIZE);
+        bytes_read = fread(outdata, 1, AES_BLOCK_SIZE, ifp);
+        AES_cfb128_encrypt(outdata, decryptdata, bytes_read, &key, ivec, &postion, AES_DECRYPT);
+        bytes_write = fwrite(decryptdata, 1, bytes_read, ofp);
+        if (bytes_read < AES_BLOCK_SIZE)
+            break;
+    }
+    fclose(ifp);
+    fclose(ofp);
 }
 
 int main(void) {
-	AES_set_encrypt_key(userkey, 128, &key);
-	encrypt();
-	decrypt();
+    AES_set_encrypt_key(userkey, 128, &key);
+    encrypt();
+    decrypt();
 }
